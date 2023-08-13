66c78bf070545f0efdbf4586dd8217a91ce289f2
abdogamal90 <abdogamalrahim@gmail.com>
2023-08-13 17:15:40 +0300

    as

--- src/components/ChannelDetails.jsx
+++ src/components/ChannelDetails.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { fetchApi } from "../others/api";
 import Videos from "./Videos";
 import { Box, Card, CardMedia } from "@mui/material";
-import Logo from "../others/logo192.png"
+import Logo from "../others/logo192.png";
 
 const ChannelDetails = () => {
   const { id } = useParams();
@@ -21,19 +21,27 @@ const ChannelDetails = () => {
   }, [id]);
   return (
     <div>
-
-      <Card sx={{ background:'black', display:'flex' , justifyContent:'center', flexDirection:'column', alignItems:'center'}}>
+      <Card
+        sx={{
+          background: "black",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <CardMedia
           sx={{ height: "180px", borderRadius: "50%", width: "180px" }}
           component="img"
           image={channelDetails.snippet?.thumbnails?.high?.url || Logo}
         />
-        <h1 style={{color:'white'}}>{channelDetails.snippet?.title}</h1>
+        <h1 style={{ color: "white" }}>{channelDetails.snippet?.title}</h1>
       </Card>
 
-      <Box sx={{ml:'60px'}}>
+      <Box sx={{ ml: "60px" }}>
         <Videos videos={videos} />
       </Box>
+      <div>als;dak;sd</div>
     </div>
   );
 };
